16bebe4d529caffd4e7698c4773afc4eac494e12
Yuya Imoto <imoto.yuya@icloud.com>
2019-05-29 23:01:43 +0900

    Initial Commit

---
+++ IdeaMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IdeaMemo
+//
+//  Created by Yuya Imoto on 2019/05/29.
+//  Copyright © 2019 Yuya Imoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
